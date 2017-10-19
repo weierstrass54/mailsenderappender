4da8e55f10471948803fccd780987f9601cd99b8
Валентин Кибалов <v.kibalov@e2e4online.ru>
2017-10-19 12:17:34 +0700

    API-242 Mailsender Appender Разделение по классам

--- src/main/java/ru/opentech/logback/mailsender/MailsenderAppender.java
+++ src/main/java/ru/opentech/logback/mailsender/MailsenderAppender.java
@@ -3,38 +3,23 @@ package ru.opentech.logback.mailsender;
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Layout;
-import ch.qos.logback.core.UnsynchronizedAppenderBase;
-import ch.qos.logback.core.db.ConnectionSource;
 import ch.qos.logback.core.pattern.PatternLayoutBase;
 
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MailsenderAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
+public class MailsenderAppender extends MailsenderAppenderBase<ILoggingEvent> {
 
     private static final String DEFAULT_SUBJECT_PATTERN = "%logger{20} - %m";
-    private static final int DEFAULT_PRIORITY = 10;
-
-    // соединения к СУБД (из пула или из драйвера)
-    private ConnectionSource connectionSource;
 
     // шаблон писем
     private Layout<ILoggingEvent> layout;
 
     // шаблон темы
     private Layout<ILoggingEvent> subjectLayout;
-
-    // источник письма (см. OtMailsender)
-    private String source;
-    // приоритет письма (см. OtMailSender)
-    private int priority;
 
     // строка темы (шаблон или просто строка)
     private String subjectStr;
@@ -47,139 +32,11 @@ public class MailsenderAppender extends UnsynchronizedAppenderBase<ILoggingEvent
 
     @Override
     public void start() {
-        if( connectionSource == null ) {
-            throw new IllegalStateException( "Не указан connectionSource для MailsenderAppender" );
-        }
         if( from == null ) {
             throw new IllegalStateException( "Не указан адресант для MailsenderAppender" );
         }
         subjectLayout = buildSubjectLayout( subjectStr );
-        if( priority == 0 ) {
-            priority = DEFAULT_PRIORITY;
-        }
-        if( source == null ) {
-            source = "UNKNOWN_SOURCE";
-        }
         super.start();
-    }
-
-    @Override
-    protected void append( ILoggingEvent event ) {
-        try(
-            Connection connection = connectionSource.getConnection()
-        ) {
-            connection.setAutoCommit( false );
-
-            int bodyId;
-            // синхронизируемся здесь, чтобы разные потоки не перехватывали bodyId
-            synchronized( this ) {
-                bodyId = insertMailBody( connection, event );
-            }
-            insertMailHeader( connection, bodyId, event );
-
-            connection.commit();
-        }
-        catch( SQLException e ) {
-            addError( "Ошибка записи события в СУБД", e );
-        }
-    }
-
-    /**
-     * Создание шаблона темы письма из заданного в настройках
-     * @param subjectStr шаблон темы письма в виде строки
-     * @return шаблон темы для logback
-     */
-    private Layout<ILoggingEvent> buildSubjectLayout( String subjectStr ) {
-        if( subjectStr == null ) {
-            subjectStr = DEFAULT_SUBJECT_PATTERN;
-        }
-        PatternLayout patternLayout = new PatternLayout();
-        patternLayout.setContext( getContext() );
-        patternLayout.setPattern( subjectStr );
-        patternLayout.setPostCompileProcessor( null );
-        patternLayout.start();
-        return patternLayout;
-    }
-
-    /**
-     * Добавление в СУБД тела письма
-     * @param connection соединение к СУБД
-     * @param event событие лога
-     * @return идентификатор тела письма
-     * @throws SQLException в случае ошибки
-     */
-    private int insertMailBody( Connection connection, ILoggingEvent event ) throws SQLException {
-        ResultSet resultSet = null;
-        try(
-            PreparedStatement statement = connection.prepareStatement(
-                "INSERT INTO mailsender.body( sender, subject, body ) VALUES ( ?, ?, ? ) RETURNING id"
-            )
-        ) {
-            statement.setString( 1, from );
-            statement.setString( 2, buildSubject( event ) );
-            statement.setString( 3, buildBody( event ) );
-            resultSet = statement.executeQuery();
-            if( !resultSet.next() ) {
-                throw new SQLException( "Не удалось добавить тело письма. Запрос вернул пустоту" );
-            }
-            return resultSet.getInt( 1 );
-        }
-        finally {
-            if( resultSet != null ) {
-                resultSet.close();
-            }
-        }
-    }
-
-    /**
-     * Добавление заголовка письма в СУБД
-     * @param connection соединение к СУБД
-     * @param bodyId идентификатор тела письма
-     * @param event событие лога
-     * @throws SQLException в случае ошибки
-     */
-    private void insertMailHeader( Connection connection, int bodyId, ILoggingEvent event ) throws SQLException {
-        try(
-            PreparedStatement statement = connection.prepareStatement(
-                "INSERT INTO mailsender.head( id_body, recipient, source, priority ) VALUES ( ?, ?, ?, ? )"
-            )
-        ) {
-            statement.setInt( 1, bodyId );
-            statement.setString( 3, source );
-            statement.setInt( 4, priority );
-            List<InternetAddress> addresses = parseAddress( event );
-            if( addresses.isEmpty() ) {
-                throw new SQLException( "Список почтовых ящиков пуст. Прерываю передачу." );
-            }
-            for( InternetAddress recipient : parseAddress( event ) ) {
-                statement.setString( 2, recipient.toString() );
-                statement.executeUpdate();
-            }
-        }
-    }
-
-    /**
-     * Получение адресатов из шаблона, указанного в настройках
-     * @param event событие лога
-     * @return список почтовых ящиков, на которые надо отправить письмо
-     */
-    private List<InternetAddress> parseAddress( ILoggingEvent event ) {
-        List<InternetAddress> addresses = new ArrayList<>();
-        for( PatternLayoutBase<ILoggingEvent> email : toPatternLayoutList ) {
-            try {
-                String address = email.doLayout( event );
-                if( address == null || address.length() == 0 ) {
-                    continue;
-                }
-                InternetAddress[] buffer = InternetAddress.parse( address, true );
-                addresses.addAll( Arrays.asList( buffer ) );
-            }
-            catch( AddressException e ) {
-                addError( "Не удалось распознать email адресата", e );
-                return addresses;
-            }
-        }
-        return addresses;
     }
 
     /**
@@ -187,7 +44,8 @@ public class MailsenderAppender extends UnsynchronizedAppenderBase<ILoggingEvent
      * @param event событие лога
      * @return тема в виде строки, построенной по шаблону
      */
-    private String buildSubject( ILoggingEvent event ) {
+    @Override
+    protected String buildSubject( ILoggingEvent event ) {
         String subject = "Тема не указана";
         if( subjectLayout != null ) {
             subject = subjectLayout.doLayout( event );
@@ -200,12 +58,8 @@ public class MailsenderAppender extends UnsynchronizedAppenderBase<ILoggingEvent
         return subject;
     }
 
-    /**
-     * Формирование тела письма из события лога
-     * @param event событие лога
-     * @return тело письма в виде строки
-     */
-    private String buildBody( ILoggingEvent event ) {
+    @Override
+    protected String buildBody( ILoggingEvent event ) {
         StringBuilder buffer = new StringBuilder();
 
         String header = layout.getFileHeader();
@@ -229,12 +83,41 @@ public class MailsenderAppender extends UnsynchronizedAppenderBase<ILoggingEvent
         return buffer.toString();
     }
 
-    public ConnectionSource getConnectionSource() {
-        return connectionSource;
+    @Override
+    protected List<InternetAddress> parseAddress( ILoggingEvent event ) {
+        List<InternetAddress> addresses = new ArrayList<>();
+        for( PatternLayoutBase<ILoggingEvent> email : toPatternLayoutList ) {
+            try {
+                String address = email.doLayout( event );
+                if( address == null || address.length() == 0 ) {
+                    continue;
+                }
+                InternetAddress[] buffer = InternetAddress.parse( address, true );
+                addresses.addAll( Arrays.asList( buffer ) );
+            }
+            catch( AddressException e ) {
+                addError( "Не удалось распознать email адресата", e );
+                return addresses;
+            }
+        }
+        return addresses;
     }
 
-    public void setConnectionSource( ConnectionSource connectionSource ) {
-        this.connectionSource = connectionSource;
+    /**
+     * Создание шаблона темы письма из заданного в настройках
+     * @param subjectStr шаблон темы письма в виде строки
+     * @return шаблон темы для logback
+     */
+    private Layout<ILoggingEvent> buildSubjectLayout( String subjectStr ) {
+        if( subjectStr == null ) {
+            subjectStr = DEFAULT_SUBJECT_PATTERN;
+        }
+        PatternLayout patternLayout = new PatternLayout();
+        patternLayout.setContext( getContext() );
+        patternLayout.setPattern( subjectStr );
+        patternLayout.setPostCompileProcessor( null );
+        patternLayout.start();
+        return patternLayout;
     }
 
     public Layout<ILoggingEvent> getLayout() {
@@ -245,14 +128,6 @@ public class MailsenderAppender extends UnsynchronizedAppenderBase<ILoggingEvent
         this.layout = layout;
     }
 
-    public void setSource( String source ) {
-        this.source = source;
-    }
-
-    public void setPriority( int priority ) {
-        this.priority = priority;
-    }
-
     public String getSubject() {
         return subjectStr;
     }
@@ -261,12 +136,13 @@ public class MailsenderAppender extends UnsynchronizedAppenderBase<ILoggingEvent
         this.subjectStr = subjectStr;
     }
 
-    public String getFrom() {
-        return from;
-    }
-
     public void setFrom( String from ) {
         this.from = from;
+    }
+
+    @Override
+    public String getFrom() {
+        return from;
     }
 
     public void addTo( String to ) {
